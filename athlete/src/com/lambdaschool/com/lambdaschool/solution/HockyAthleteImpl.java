ea54ba1f33d25c6e6d1bd28f2e2331ee2940773a
Lindsey Cason <lindseyacason@gmail.com>
2019-10-03 20:31:01 -0500

    MVP

---
+++ athlete/src/com/lambdaschool/com/lambdaschool/solution/HockyAthleteImpl.java
@@ -0,0 +1,10 @@
+package com.lambdaschool.solution;
+
+public class HockyAthleteImpl implements AthleteCreation
+{
+    @Override
+    public void displayAthlete()
+    {
+        System.out.println("HOCKEY PLAYER-- HAPPY GILMORE");
+    }
+}
